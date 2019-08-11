044b512b121471c58227085bd00f2abd550ec97f
Cokemonkey11 <Cokemonkey11@gmail.com>
2019-08-11 15:26:21 +0100

    adjusted base layout/terrain.  Closes #91.

--- .generate_submission_template.py
+++ .generate_submission_template.py
@@ -66,7 +66,7 @@ CHANGELOG = (
         "11 Aug 2019",
         (
             "Hopefully fixed a bug where Flight Mode was undisruptible.",
-            
+            "Added more turrets and shields; adjusted base terrain.",
         )
     ),
     (
